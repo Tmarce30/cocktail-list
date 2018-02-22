d6afd5a1972dd0b23bb3d6bcf3aa196523ef2eeb
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-22 23:05:02 +0100

    added destroy method to dose

--- cocktail-list/app/controllers/doses_controller.rb
+++ cocktail-list/app/controllers/doses_controller.rb
@@ -1,4 +1,5 @@
 class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
   before_action :set_cocktail, only: [:new, :create]
 
   def new
@@ -15,6 +16,12 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
   private
 
   def dose_params
@@ -22,6 +29,10 @@ class DosesController < ApplicationController
   end
 
   private
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
 
   def set_cocktail
     @cocktail = Cocktail.find(params[:cocktail_id])
