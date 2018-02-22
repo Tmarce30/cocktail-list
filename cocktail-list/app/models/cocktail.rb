8ad735c4cc918c4fac1d905215affc3181e71e83
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-22 20:28:17 +0100

    added validations to cocktail model

--- cocktail-list/app/models/cocktail.rb
+++ cocktail-list/app/models/cocktail.rb
@@ -1,2 +1,7 @@
 class Cocktail < ApplicationRecord
+  has_many :doses
+
+  validates :name, uniqueness: true, presence: true
+  # validates :description, presence: true
+  # validates :recipe, presence: true
 end
