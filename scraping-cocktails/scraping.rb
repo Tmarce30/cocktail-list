60954b20078b31594cc185317d65e2530d9e61ed
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-23 23:24:58 +0100

    added method to scrap cocktail recipes

--- scraping-cocktails/scraping.rb
+++ scraping-cocktails/scraping.rb
@@ -1,17 +1,30 @@
 require 'nokogiri'
 require 'csv'
 
+def scrap_cocktail_index
+  html_file = open('cocktail-html-source.htm').read
+  html_doc = Nokogiri::HTML(html_file)
 
-html_file = open('cocktail-html-source.htm').read
-html_doc = Nokogiri::HTML(html_file)
+  csv_options = { write_headers: true, headers: ["name","ingredients"] }
 
-csv_options = { write_headers: true, headers: ["name","ingredients"] }
+  CSV.open("cocktails.csv", 'w', csv_options) do |csv|
+    html_doc.search('.recipe_summary.pjax').each do |element|
+      name = element.search('h3').text
+      ingredients = element.search('.content-appear').search('p').first.text
 
-CSV.open("cocktails.csv", 'w', csv_options) do |csv|
-  html_doc.search('.recipe_summary.pjax').each do |element|
-    name = element.search('h3').text
-    ingredients = element.search('.content-appear').search('p').first.text
-
-    csv << [name, ingredients]
+      csv << [name, ingredients]
+    end
   end
 end
+
+def scrap_cocktail_pages
+  # "https://www.socialandcocktail.co.uk/cocktails/mojito"
+
+  html_file = open('mojit-cocktail.htm').read
+  html_doc = Nokogiri::HTML(html_file)
+
+  html_doc.search('.recipe-content').each do |element|
+    p element.search('p')[1].text.strip
+  end
+end
+
