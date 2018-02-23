adec2753d2848dde40c1c9801d9d15aeee184506
Thibaud Marcé <thibaud.marce@gmail.com>
2018-02-23 23:05:26 +0100

    added headers when writing csv

--- scraping-cocktails/scraping.rb
+++ scraping-cocktails/scraping.rb
@@ -5,7 +5,9 @@ require 'csv'
 html_file = open('cocktail-html-source.htm').read
 html_doc = Nokogiri::HTML(html_file)
 
-CSV.open("cocktails.csv", 'w') do |csv|
+csv_options = { write_headers: true, headers: ["name","ingredients"] }
+
+CSV.open("cocktails.csv", 'w', csv_options) do |csv|
   html_doc.search('.recipe_summary.pjax').each do |element|
     name = element.search('h3').text
     ingredients = element.search('.content-appear').search('p').first.text
